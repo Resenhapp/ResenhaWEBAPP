73a70ae1e78f105cc4c39244a3343a574db6f40f
João Davi <joaodavisn@gmail.com>
2023-06-24 19:09:53 -0300

    Update perfil
    Fiz as implememtações de edição de perfil:
    Modal de edição de nome;
    Modal de edição de sobre;
    Modal de edição de interesses;
    Falta ainda resolver alguns problemas na edição de interesses mas já termino.

--- src/components/Interest.js
+++ src/components/Interest.js
@@ -1,28 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Vector from './Vector';
 
-const Interest = ({ interestIndex, isRemovable, removeEvent }) => {
-    const labels = ['Jogar no PC','Eventos','Música','Arte','Fimes','Outros'];
+const Interest = ({ interestIndex, isRemovable, removeEvent, isAppend, appendEvent }) => {
+    const labels = ['Gamer','Eventos','Música','Arte','Fimes','Outros', 'Anime', 'Cultura', 'Baile Funk', 'Festa Temática', 'LGBTQ+', 'Nerd', 'F1', 'Pool Party', 'Novas experiências'];
+    const [isSelected, setIsSelected] = useState(false);
 
     let className;
     switch(interestIndex) {
         case 1:
-            className = "bg-purpleT3";
+            className = "bg-blueT3";
             break;
         case 2:
             className = "bg-redT3";
             break;
         case 3:
-            className = "bg-orangeT3";
+            className = "bg-orangeT2";
             break;
         case 4:
             className = "bg-blueT3";
             break;
         case 5:
-            className = "bg-blueT4";
+            className = "bg-yellowT2";
             break;
         case 6:
-            className = "bg-greenT3";
+            className = "bg-grayT0";
+            break;
+        case 7:
+            className = "bg-redT3";
+            break;
+        case 8:
+            className = "bg-orangeT2";
+            break;
+        case 9:
+            className = "bg-blueT2";
+            break;
+        case 10:
+            className = "bg-yellowT2";
+            break;
+        case 11:
+            className = "bg-gradi font-bold text-[#ffffff]";
+            break;
+        case 12:
+            className = "bg-blueT3";
+            break;
+        case 13:
+            className = "bg-greenT2";
+            break;
+        case 14:
+            className = "bg-blueT2";
+            break;
+        case 15:
+            className = "bg-orangeT2";
             break;
         default:
             className = "bg-default";
@@ -32,14 +60,19 @@ const Interest = ({ interestIndex, isRemovable, removeEvent }) => {
     const label = labels[interestIndex - 1];
     
     const handleClick = () => {
-        if (isRemovable) {
+        if (isAppend) {
+            setIsSelected(!isSelected);
+            if(!isSelected) {
+                appendEvent(label);
+            }
+        } else if (isRemovable) {
             removeEvent();
         }
     }
 
     return (
-        <button onClick={handleClick} className={`${className} w-fit flex flex-row items-center gap-1 h-fit py-2 px-4 rounded-full`}>
-            {isRemovable && <Vector vectorname={'xmark01'} />}
+        <button onClick={handleClick} className={`${className} w-fit flex flex-row items-center gap-1 h-fit py-2 px-4 rounded-full ${isSelected ? 'ring-2 ring-inset ring-[#FFFFFF80]' : ''}`}>
+            {(isRemovable || isAppend) && <Vector vectorname={isSelected ? 'minus01' : (isAppend ? 'plus01' : 'xmark01')} />}
             <h1 className='text-sm'>{label}</h1>
         </button>
     )
