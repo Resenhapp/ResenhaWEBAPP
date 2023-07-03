a766041adaa86a99fa2672f15147decb9725e9a7
Vitor Prates <cadastro@vitorprates.com>
2023-07-03 18:10:07 -0300

    Party display removido.
    Substitui o party display por apenas a instancia do PartyBanner de forma dinamica.

--- src/components/PartyBanner.js
+++ src/components/PartyBanner.js
@@ -2,15 +2,22 @@ import React from 'react';
 import RoundButton from './RoundButton';
 import Vector from './Vector';
 
-const PartyBanner = ({ eventName, eventGuests, eventMax, eventImage, eventPrice}) => {
+const PartyBanner = ({ eventName, eventGuests, eventMax, eventImage, eventPrice, eventSaved}) => {
     const handleNavigation = (pageToGo) => {
         window.location.href = `/webapp/${pageToGo}`;
     };
+
+    var vectorSaved = 'bookmarkOutlined01';
+
+    if (eventSaved) {
+        vectorSaved = 'bookmarkOutlined01';
+    }
+
     return (
-        <div className='flex flex-col items-center'>
-            <div className="relative z-1 flex flex-row items-end w-full justify-between h-[29vh] ring-1 ring-inset bg-gradient-to-t from-purpleT0 ring-whiteT1 rounded-2xl">
+        <div className='flex flex-col items-center mt-3'>
+            <div className="relative z-1 flex flex-row items-end w-full justify-between h-[29vh] ring-1 ring-inset bg-gradient-to-t from-purpleT0 ring-purpleT4 rounded-2xl">
                 <div className='w-full absolute h-full p-2 flex flex-row-reverse'>
-                    <button onClick={() => handleNavigation('resenhas/convites/')} className='w-fit h-fit py-1 gap-2 font-thin px-3 rounded-full ring-1 ring-inset ring-whiteT1 flex flex-row justify-center items-center content-center backdrop-blur-xl bg-[#0000004D]'><Vector vectorname={'arrowRight05'}/></button>
+                    <button onClick={() => handleNavigation('resenhas/convites/')} className='w-8 h-8 gap-2 font-thin rounded-full ring-1 ring-inset ring-whiteT1 flex flex-row justify-center items-center content-center backdrop-blur-xl bg-[#0000004D]'><Vector vectorname={vectorSaved}/></button>
                 </div>
                 <img src={eventImage} className='absolute top-0 left-0 rounded-2xl z-[-1] object-cover w-full h-full' />
                 <div className='p-4'>
