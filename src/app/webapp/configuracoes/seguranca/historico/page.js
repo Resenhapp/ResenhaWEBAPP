f785887f8b9c9fe932481053d8de761fb716eb93
Buuhzerah <59845679+Buuhzerah@users.noreply.github.com>
2023-07-04 23:23:31 -0300

    Segurança - Histórico Feito
    Tela de configurações/segurança/histórico completa
    
    Feito o componente SessionNotification.js

--- src/app/webapp/configuracoes/seguranca/historico/page.js
+++ src/app/webapp/configuracoes/seguranca/historico/page.js
@@ -1,22 +1,39 @@
 'use client'
 import React from 'react';
 import PageHeader from '@/src/components/PageHeader';
+import SessionNotification from '@/src/components/SessionNotification';
 
 export const metadata = {
     title: 'Resenha.app • Histórico de sessões',
     description: 'Venha fazer suas resenhas!',
 }
 
-export default function Privacy() {
-
-    const handleNavigation = (pageToGo) => {
-        window.location.href = `/webapp/${pageToGo}`;
-    };
-
+export default function SecurityHistory() {
     return (
-        <div>
-
+        <div className='flex flex-col w-screen h-screen'>
+            <PageHeader isBack={true} checker={() => { null }} pageTitle="Histórico de sessões" />
+            <div className="flex flex-col items-center justify-start h-screen px-4">
+                <section className="flex w-full max-w-md p-4">
+                    <div className='h3 w-full flex'>
+                        <div className='w-full flex flex-col'>
+                            <div className='h-fit w-full gap-2 flex flex-col pt-4'>
+                                <SessionNotification
+                                    title={'Nova sessão iniciada'}
+                                    description={'Detectamos um novo login utilizando as suas credenciais na região de Porto Alegre, Brasil.'}
+                                    date={'Hoje'}
+                                    hour={'21:34'}
+                                />
+                                <SessionNotification
+                                    title={'Nova sessão iniciada'}
+                                    description={'Detectamos um novo login utilizando as suas credenciais na região de Belém, Pará.'}
+                                    date={'09 de Junho'}
+                                    hour={'09:34'}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
         </div>
-    )
-
+    );
 }
