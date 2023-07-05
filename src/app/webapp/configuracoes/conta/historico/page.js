cdadae2c234a3b08ef74218b766ffbcb9b31c670
Buuhzerah <59845679+Buuhzerah@users.noreply.github.com>
2023-07-04 22:27:28 -0300

    Configurações - Conta - Finalizada

--- src/app/webapp/configuracoes/conta/historico/page.js
+++ src/app/webapp/configuracoes/conta/historico/page.js
@@ -1,8 +1,6 @@
 'use client'
-import React, { useState } from 'react';
+import React from 'react';
 import PageHeader from '@/src/components/PageHeader';
-import Modal from '@/src/components/Modal';
-import ButtonConfig from '@/src/components/ButtonConfig';
 import NotificationBase from '@/src/components/NotificationBase';
 
 export const metadata = {
@@ -11,53 +9,10 @@ export const metadata = {
 };
 
 export default function AccountHistory() {
-    const [username, setUsername] = useState('@dabilas');
-    const [tempUsername, setTempUsername] = useState(username);
-
-    const [email, setEmail] = useState('joao*****n@gmail.com')
-    const [tempEmail, setTempEmail] = useState(email)
-
-    const [isUnsavedChangesModalOpen, setUnsavedChangesModalOpen] = useState(false);
-    const [checkerCallback, setCheckerCallback] = useState(null);
-
-    const [hasChange, setHasChange] = useState(false);
-
-    const handleUsernameChange = (newValue) => {
-        setHasChange(true);
-        setTempUsername(newValue);
-    };
-
-    const handleEmailChange = (newValue) => {
-        setHasChange(true);
-        setTempEmail(newValue);
-    };
-
-    const handleCancel = () => {
-        setHasChange(false);
-        setTempUsername(username);
-        setTempEmail(email);
-    }
-
-    const handleSave = () => {
-        setHasChange(false);
-        setUsername(tempUsername);
-        setEmail(tempEmail);
-    }
 
     return (
         <div className="flex flex-col w-screen h-screen">
-            <PageHeader
-                pageTitle="Histórico de atividades"
-                isBack={true}
-                checker={() => new Promise((resolve, reject) => {
-                    if (hasChange) {
-                        setUnsavedChangesModalOpen(true);
-                        setCheckerCallback(() => resolve);
-                    } else {
-                        resolve(true);
-                    }
-                })}
-            />
+            <PageHeader isBack={true} checker={() => { null }} pageTitle="Histórico de atividades" />
             <div className="flex flex-col items-center justify-start h-screen px-4 py-4">
                 <section className="flex w-full max-w-md p-4">
                     <div className="h3 w-full flex">
@@ -82,28 +37,6 @@ export default function AccountHistory() {
                     </div>
                 </section>
             </div>
-            <div className='flex flex-row justify-center p-8'>
-                {hasChange && (
-                    <div className="flex p-8 space-x-4">
-                        <ButtonConfig label="Cancelar" action={handleCancel} />
-                        <ButtonConfig label="Salvar" action={handleSave} />
-                    </div>
-                )}
-            </div>
-            <Modal show={isUnsavedChangesModalOpen} close={setUnsavedChangesModalOpen}>
-                <div className="gap-2 flex flex-col">
-                    <h1 className="text-center">Ei! Você tem alterações que não foram salvas! Vai sair sem salvar?</h1>
-                    <button className="bg-purpleT2 ring-1 ring-purpleT3 rounded-full ring-inset py-2 px-4" onClick={() => {
-                        if (typeof checkerCallback === 'function') {
-                            checkerCallback(false);
-                        }
-                        setUnsavedChangesModalOpen(false);
-                    }}>Sim, eu vou.</button>
-                    <button className='bg-purpleT2 ring-1 ring-purpleT3 rounded-full ring-inset py-2 px-4' onClick={() => {
-                        setUnsavedChangesModalOpen(false);
-                    }}>Não, peraí!</button>
-                </div>
-            </Modal>
         </div>
     );
 }
