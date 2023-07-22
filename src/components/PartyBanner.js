09c2e5d2d31ed4ceed2acefa5353f4dcf073cb2c
Vitor Prates <cadastro@vitorprates.com>
2023-07-22 17:33:45 -0300

    Hotfix

--- src/components/PartyBanner.js
+++ src/components/PartyBanner.js
@@ -6,8 +6,6 @@ import { partyData } from '@/src/components/PartyData';
 import BookmarkButton from './SaveButton';
 
 const PartyBanner = ({imageUrl, eventCode, eventName, eventGuests, eventMax, eventImage, eventPrice, eventSaved, eventTags, handleSaveButton}) => {
-    
-
     var handleClick = (pageToGo) => {
         if (typeof window !== 'undefined') {
             window.location.href = `/convite/?code=${pageToGo}`;
@@ -15,7 +13,7 @@ const PartyBanner = ({imageUrl, eventCode, eventName, eventGuests, eventMax, eve
     };
 
     return (
-        <div className='flex flex-col items-center mt-3'>
+        <div onClick={() => handleClick(eventCode)} className='flex flex-col items-center mt-3'>
             <div className="relative z-1 flex flex-row items-end w-full justify-between h-[29vh] ring-1 ring-inset bg-gradient-to-t from-black ring-purpleT4 rounded-2xl">
                 <div className='w-full absolute h-fit top-0 p-2 flex justify-between flex-row'>
                     <div className='flex flex-row w-full gap-1'>
@@ -26,13 +24,13 @@ const PartyBanner = ({imageUrl, eventCode, eventName, eventGuests, eventMax, eve
                         ))
                         }
                     </div>
-                    <BookmarkButton initialSavedState={handleSaveButton} />
+                    <BookmarkButton initialSavedState={handleSaveButton}/>
                 </div>
                 <img src={eventImage} className='absolute top-0 left-0 rounded-2xl z-[-1] object-cover w-full h-full' alt='P Banner' />
                 <div className='py-4 pl-4'>
                     <h1 className="font-bold text-xl text-white overflow-hidden overflow-ellipsis whitespace-nowrap max-w-[200px]">{eventName}</h1>
                     <div className='flex flex-row gap-2 items-center'>
-                        <div className='flex flex-row' onClick={() => handleClick(eventCode)}>
+                        <div className='flex flex-row'>
                             {imageUrl.map((url, index) => (
                                 <div key={index.id}>
                                     <Image
