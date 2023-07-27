eff09091e3faa29a0e60283f3d780a1b7f75da78
João Davi <joaodavisn@gmail.com>
2023-07-26 23:17:45 -0300

    UX update - criar resenha

--- api/default.php
+++ api/default.php
@@ -1,5 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Origin: https://api.resenha.app/');
 header('Access-Control-Allow-Methods: GET, POST');
 header('Access-Control-Allow-Headers: Content-Type');
 
