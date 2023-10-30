00d00192a37d16a9d87c8186b7a6b268a92fbffe
Humberto Corrêa <humberto.cgomes17@gmail.com>
2023-10-30 12:32:35 -0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from "next/image";
 import Vector from "../components/Vector";
 import Logo from "@/assets/images/logo.png";
@@ -14,16 +14,31 @@ import page3_desktop from '@/assets/images/page3_desktop.png';
 import page4_desktop from '@/assets/images/page4_desktop.png';
 import page5_desktop from '@/assets/images/page5_desktop.png';
 import Button from '../components/Button';
+
 export default function Home() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
-  const isDesktop = window.innerWidth > 1024;
+  const [isDesktop, setIsDesktop] = useState(false);
+
+  useEffect(() => {
+    setIsDesktop(window.innerWidth > 1024);
+
+    //listener para atualizar a largura quando a janela for redimensionada
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth > 1024);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <main className="">
       <div className=' h-screen w-screen'>
         <nav className='w-full h-fit p-10 flex flex-row justify-between absolute'>
-        <Vector vectorname={'thunder07'} />
-
+          <Vector vectorname={'thunder07'} />
           <button onClick={() => setSidebarOpen(true)} className=''>
             <Vector vectorname={'burguer02'} />
           </button>
@@ -38,43 +53,42 @@ export default function Home() {
           />
         </section>
         <section>
-        <Image
-              src={isDesktop ? page2_desktop : page2}
-              width={isDesktop ? 2740 : 1080}
-              height={isDesktop ? 1540 : 1920}
-              alt='background'
-              className='w-full h-screen object-cover z-0 mt-[-1px] max-w-[1500px]'
-            />
+          <Image
+            src={isDesktop ? page2_desktop : page2}
+            width={isDesktop ? 2740 : 1080}
+            height={isDesktop ? 1540 : 1920}
+            alt='background'
+            className='w-full h-screen object-cover z-0 mt-[-1px] max-w-[1500px]'
+          />
         </section>
         <section>
-        <Image
-              src={isDesktop ? page3_desktop : page3}
-              width={isDesktop ? 2740 : 1080}
-              height={isDesktop ? 1540 : 1920}
-              alt='background'
-              className='w-full h-screen object-cover z-0 mt-[-1px] max-w-[1500px]'
-            />
+          <Image
+            src={isDesktop ? page3_desktop : page3}
+            width={isDesktop ? 2740 : 1080}
+            height={isDesktop ? 1540 : 1920}
+            alt='background'
+            className='w-full h-screen object-cover z-0 mt-[-1px] max-w-[1500px]'
+          />
         </section>
         <section>
-        <Image
-              src={isDesktop ? page4_desktop : page4}
-              width={isDesktop ? 2740 : 1080}
-              height={isDesktop ? 1540 : 1920}
-              alt='background'
-              className='w-full h-screen object-cover z-0 mt-[-1px] max-w-[1500px]'
-            />
+          <Image
+            src={isDesktop ? page4_desktop : page4}
+            width={isDesktop ? 2740 : 1080}
+            height={isDesktop ? 1540 : 1920}
+            alt='background'
+            className='w-full h-screen object-cover z-0 mt-[-1px] max-w-[1500px]'
+          />
         </section>
         <section className='relative'>
-        <Image
-              src={isDesktop ? page5_desktop : page5}
-              width={isDesktop ? 2740 : 1080}
-              height={isDesktop ? 1540 : 1920}
-              alt='background'
-              className='w-full h-screen object-cover z-0 mt-[-1px] max-w-[1500px]'
-              
-/>
-          <button className='absolute z-4 bg-purpleT3 hover:bg-purpleT2 left-[15%] px-24 rounded-xl truncate py-8 bottom-72' onClick={() => window.location.href = 'https://resenha.app/cadastro'}>Criar conta</button>
-          <footer className='absolute bottom-0 mb-12'> 
+          <Image
+            src={isDesktop ? page5_desktop : page5}
+            width={isDesktop ? 2740 : 1080}
+            height={isDesktop ? 1540 : 1920}
+            alt='background'
+            className='w-full h-screen object-cover z-0 mt-[-1px] max-w-[1500px]'
+          />
+          <button className='absolute z-4 bg-purpleT3 hover-bg-purpleT2 left-[15%] px-24 rounded-xl truncate py-8 bottom-72' onClick={() => window.location.href = 'https://resenha.app/cadastro'}>Criar conta</button>
+          <footer className='absolute bottom-0 mb-12'>
             <p className='absolute bottom-0 w-screen text-center text-sm'>Resenha.app® 2023 - Todos os direitos reservados.</p>
           </footer>
         </section>
@@ -84,10 +98,10 @@ export default function Home() {
 
         {/* Sidebar */}
         <div className={`fixed inset-y-0 right-0 w-full bg-purpleT0 max-w-md z-30 transform transition-transform ease-in-out duration-500 ${sidebarOpen ? 'translate-x-0' : 'translate-x-full'}`}>
-        <div className='flex flex-col py-20 px-4 flex-start gap-8 mt-12'>
-          <button onClick={() => window.location.href = 'https://resenha.app/cadastro'} className='text-2xl px-8 py-2 text-purpleT4 rounded-xl'>Criar conta</button>
+          <div className='flex flex-col py-20 px-4 flex-start gap-8 mt-12'>
+            <button onClick={() => window.location.href = 'https://resenha.app/cadastro'} className='text-2xl px-8 py-2 text-purpleT4 rounded-xl'>Criar conta</button>
             <button onClick={() => window.location.href = 'https://resenha.app/login'} className='text-2xl px-8 py-2 text-purpleT4 rounded-xl'>Fazer login</button>
-        </div>
+          </div>
           <div className='absolute top-0 left-0 p-8'>
             <button onClick={() => setSidebarOpen(false)}>
               <Vector vectorname={'xmark02'} />
@@ -97,5 +111,5 @@ export default function Home() {
         </div>
       </div>
     </main>
-  )
+  );
 }
