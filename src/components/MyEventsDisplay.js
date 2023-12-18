97e3a1993e632f8597f626bbfee1e70edbd39e53
vitor <83680381+vitor-prates@users.noreply.github.com>
2023-12-18 20:50:30 -0300

    sad

--- src/components/MyEventsDisplay.js
+++ src/components/MyEventsDisplay.js
@@ -20,7 +20,7 @@ const MyEventsDisplay = ({eventName, eventDate, eventHour, eventGuests, eventMax
                 </div>
             </div>
             <div className="flex flex-col mb-4 w-full mt-8 items-center justify-center content-center">
-                <Button label={'Nova Resenha'} icon={'plus'} action={() => handleNavigation('resenhas/criar/')} iconSide='right' height={1} width={1} textAlign='center' />
+                <Button label={'Nova resenha'} icon={'plus'} action={() => handleNavigation('resenhas/criar/')} iconSide='right' height={1} width={1} textAlign='center' />
             </div>
         </section>
     )
