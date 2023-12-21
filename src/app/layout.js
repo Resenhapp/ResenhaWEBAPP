4d2f6b832c6d28a42b1587dd9a8255f9fe54d778
Humberto Corrêa Gomes <humberto.cgomes17@gmail.com>
2023-12-21 00:43:47 -0300

    up

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,32 +1,10 @@
 // import React from 'react';
 // import { Helmet } from 'react-helmet';
 import '@/styles/globals.css';
-import React from 'react';
-
-export const metadata = {
-  title: 'Resenha',
-  date: '2023-12-25', // exemplo de data
-  guests: '100', // exemplo de número de convidados
-  hour: '18:00', // exemplo de hora
-  address: '123 Main Street, City', // exemplo de endereço
-  host: 'Anfitrião', // exemplo de nome do anfitrião
-  ticket: '25.00',
-}
 
 export default function RootLayout({ children }) {
-  const { date, guests, hour, address, host, ticket } = metadata;
   return (
     <html lang="en" className='h-full'>
-      <head>
-        <meta name="date" content={date} />
-        <meta name="guests" content={guests} />
-        <meta name="hour" content={hour} />
-        <meta name="address" content={address} />
-        <meta name="host" content={host} />
-        <meta name="ticket" content={ticket} />
-        {/* Adicionando título com base no nome do host */}
-        <title>{`Evento de ${host}`}</title>
-      </head>
       <body className='bg-purpleT0 overflow-x-hidden w-screen min-h-screenh-fit text-whiteT1'>
         <div className='opacity-100 scale-0 absolute'>
           <div className="bg-blackT1" />
