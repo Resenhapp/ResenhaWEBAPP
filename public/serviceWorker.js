6c0269300c4a28c44542ebcc030125c7400390b3
vitor <83680381+prates-wtf@users.noreply.github.com>
2024-01-09 03:02:23 -0300

    da

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'resenhapp';
-const urlsToCache = ['/', '/index.html', '/manifest.json'];
+const urlsToCache = ['/', '/manifest.json'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
