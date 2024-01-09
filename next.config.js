5651cd2b76bb40de970d6b7466208de48e5d7345
vitor <83680381+prates-wtf@users.noreply.github.com>
2024-01-09 03:06:23 -0300

    d

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['chart.googleapis.com','resenha.app', 'media.resenha.app', 'api.pagar.me'],
+      domains: ['chart.googleapis.com','resenha.app', 'media.resenha.app', 'api.pagar.me', 'api.resenha.app'],
     },
-  };
+};
   
-  module.exports = nextConfig;
+module.exports = nextConfig;
