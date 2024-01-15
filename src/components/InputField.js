d1d7cfb4457348957fbfdc47273e24d4b43e97eb
João Davi <joaodavisn@gmail.com>
2024-01-15 11:08:05 -0300

    inputs

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -5,7 +5,7 @@ const InputField = ({ placeholder, value, action, showIcon = false, Icon, Requir
   return (
     <div className="relative h-14 ring-1 ring-inset ring-whiteT2 bg-whiteT1 rounded-full flex items-center">
       {showIcon && Icon && (
-        <div className="absolute left-0 pl-3 flex">
+        <div className="absolute left-0 pl-4 flex">
           {Icon === 'lock' ? (
             <Vector vectorname={'lock01'} />
           ) : Icon === 'mail' ? (
