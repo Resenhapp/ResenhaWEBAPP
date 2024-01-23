983e51f579b6e72b30bd009e918735ad5ddc636c
Vitor <cadastro@vitorprates.com>
2024-01-23 04:13:12 -0300

    Update Loading.js

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,15 +1,7 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Vector from './Vector';
 
 const Loading = () => {
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            window.location.href = '/login';
-        }, 5000);
-
-        return () => clearTimeout(timer);
-    }, []);
-
     return (
         <div className="animate-ping">
             <Vector vectorname={'thunder02'} />
