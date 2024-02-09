d5defde1ee2417c742bfabc28f6ad6b0a770397f
João Davi <joaodavisn@gmail.com>
2024-02-09 02:01:16 -0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
       <head>
       <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'/>
 
-<meta name="pwa-demo" content="pwa-demo" />
+<meta name="RESENHA APP" content="RESENHA APP" />
 <meta name="apple-mobile-web-app-capable" content="yes" />
 <meta name="apple-mobile-web-app-status-bar-style" content="default" />
 <meta name="apple-mobile-web-app-title" content="pwa-demo" />
