650e7cb7d3650add288583d99060d7d3b8525b50
João Davi <joaodavisn@gmail.com>
2024-02-09 01:55:46 -0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className='h-full'>
+    <html lang="pt_BR" className='h-full'>
       <head>
       <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'/>
 
